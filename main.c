d33d88e9d71f68694b2217ac6b2b42295fadbba9
sikari <ashish.adhikari@bison.howard.edu>
2018-03-23 01:25:13 -0400

    implemented thread entry point

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <sys/time.h>
 /* these may be any values >= 0 */
 #define NUMBER_OF_CUSTOMERS 5
 #define NUMBER_OF_RESOURCES 4
@@ -18,17 +19,11 @@ int allocation[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 int need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 
 
-
 void *customer_entry_point(void *customer_num);
-//customer function prototypes: return 0 if suc
-
-void *customer_entry_point(void *customer_num){
-	//generate max
-	
-	return 0;
-}cess -1 if failure
+//customer function prototypes: return 0 if success else -1
 int request_resources(int customer_num, int request[]);
 int release_resources(int customer_num, int release[]);
+int is_need(int cust_id);
 
 //mutexes for synchronisation across threads
 pthread_mutex_t *l_available;
@@ -45,18 +40,17 @@ int main(int argc, char**argv){
 	}else{
 		for (int i = 1; i < argc; ++i)
 		{
-			if (atoi(argv[i])<0){
+			if (atoi(argv[i])<1){
 				printf("%d\n",atoi(argv[i]) );
-				printf("Negative resource instance given, exiting...\n");
+				printf("Resource instance must be at least 1 exiting...\n");
 				return -1;
 			}
 		}
 	}
 	//populate resources into available
-	for (int i = 1; i < NUMBER_OF_RESOURCES; ++i)
-	{
+	for (int i = 1; i < NUMBER_OF_RESOURCES+1; ++i)
 		available[i]=atoi(argv[i]);
-	}
+
 	l_need = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
 	l_allocation = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
 	l_maximum = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t));
@@ -68,20 +62,57 @@ int main(int argc, char**argv){
 
 	pthread_t* customers = (pthread_t*)malloc(sizeof(pthread_t)*NUMBER_OF_CUSTOMERS);
 
-	for (int i = 0; i < NUMBER_OF_CUSTOMERS; ++i)
+	for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++)
 	{
-		pthread_create(&customers[i],NULL,customer_entry_point,(void*)&i);
+		int *id = (int*)malloc(sizeof(int));
+		*id = i;
+		pthread_create(&customers[i],NULL,customer_entry_point,(void*)id);
 	}
 
-	for (int i = 0; i < NUMBER_OF_CUSTOMERS; ++i){
+	for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++){
 		pthread_join(customers[i],NULL);
 	}
 
 	return 0;
 }
+int has_need(int cust_id){
+	int counter =0;
+	for (int i = 0; i < NUMBER_OF_RESOURCES; ++i)
+		if(need[cust_id][i]!=0) counter++;
+	int f = (counter==0)?1:0;
+	return f;
+}
+
+int request_resources(int customer_num, int request[]){
+
+}
+
+int release_resources(int customer_num, int release[]){
+
+}
 
 void *customer_entry_point(void *customer_num){
-	//generate max
+	int cust_id = *((int*)customer_num);
+	printf("%d\n",cust_id );
+	//seed random number
+	struct timeval start; 
+	gettimeofday(&start, NULL);
+	srand(start.tv_usec);  
+	while(1){
+		//populate need and max
+		for (int i = 0; i < NUMBER_OF_CUSTOMERS; ++i)
+		{
+			int temp=(rand())%(available[i]+1);  //upto max resource requestable
+			need[cust_id][i]= temp;
+			maximum[cust_id][i] = temp;
+		}
+		
+		//while need is >0, keep requesting randomly
+		while(has_need(cust_id)){
+			
+		}
+
 	
-	return 0;
+	}
+
 }
